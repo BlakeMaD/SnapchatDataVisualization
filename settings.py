071f2b68c457781af41efb7fc0042457e7ddcca6
Blake Dukes <dukes4@purdue.edu>
2021-10-07 17:15:44 -0400

    modify path if expected file not present

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@
 Settings
 """
 import os
+from os.path import exists
 
 __author__ = "Blake Dukes"
 __version__ = "0.1.0"
@@ -12,7 +13,10 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # Root path, do not change
 
 FONT_PATH = os.path.join(ROOT_DIR, 'fonts/AlexBrush-Regular.ttf')
 CLEANED_DATA_PATH = os.path.join(ROOT_DIR, 'Data_Cleaned/cleaned.csv')
-RAW_CHAT_HIST = os.path.join(ROOT_DIR, 'Data_Raw/chat_history.json')
+if exists('Data_Raw/chat_history.json'):
+    RAW_CHAT_HIST = os.path.join(ROOT_DIR, 'Data_Raw/chat_history.json')
+else:
+    RAW_CHAT_HIST = os.path.join(ROOT_DIR, 'Data_Raw/sample_chat_history.json')
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
